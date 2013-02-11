feac7cfef8711baac7f9bf9fc8580f2341768121
Bella Forrister <bella.forrister@gmail.com>
2013-02-11 18:47:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'fe980820ef7ebe9a836644b5b4afb74e3ba228cf13faf805683cb956f4bc53118582f0997cd9081b6b95e1bc800ed62f76628fb562e6fd3424a1721d2a7e707f'
---
+++ subl.bat
@@ -0,0 +1,1 @@
+start "C:\Program Files (x86)\Sublime Text 2\sublime_text.exe" %*
